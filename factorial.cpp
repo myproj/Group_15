07a0ae1fa8d4073afc227241069c31cff19c018d
Swapnil Sharma <swap.sha96@gmail.com>
2017-08-22 16:31:58 +0530

    More changes.

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,12 @@
 #include "functions.h"
 
 int factorial(int n){
+	if(n < 1) {
+		std::cout << "Please enter a positive number: ";
+		ctd::cin >> n;
+	}
+	if(n == 0)
+		return 1;
     if(n!=1){
 	return(n * factorial(n-1));
     }
