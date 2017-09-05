bc8236748e3301ca5940b844f0734d36d760286f
indresh07 <indresh768@gmail.com>
2017-09-05 15:01:29 +0530

    printf

--- factorial.cpp
+++ factorial.cpp
@@ -4,28 +4,10 @@
 
 
 int factorial(int n){
-<<<<<<< HEAD
-	if(n<0)
-	{
-	std::cout<<"Negative Number entered| Re-enter the number"<<std::endl;
-	std::cin>>n;	
-	std::cout<<"Successfully Changed!"<<std::endl;
-} 
 
-    if(n!=1){
-=======
     if(n>=1){
->>>>>>> 788297249adcd93e950007e690a83aeeb2d09595
-	return(n * factorial(n-1));
-    }
-    else if(n < 0){
-
-    	while(n < 0){
-    		std::cout<<"Negative no. entered! Re-enter n : ";
-    		std::cin>>n;
-    	}
-    	return (n * factorial(n - 1));
+	   return(n * factorial(n-1));
     }
     else
-    	return 1;
+        return 1;
 }
--- hello.cpp
+++ hello.cpp
@@ -2,5 +2,5 @@
 #include "functions.h"
 using namespace std;
 void print_hello(){
-   cout<<"Hello World!";
+   printf("Hello World!\n");
 }
